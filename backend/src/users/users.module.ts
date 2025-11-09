1e9688fca8f19cbd18c6ffa5ade39999477dd719
Michael Raoof <raoofmichael@yahoo.com>
2025-11-09 10:05:00 +0200

    feat(backend): add UsersModule (Mongoose feature + providers)

---
+++ backend/src/users/users.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User, UserSchema } from './schemas/user.schema';
+import { AuthModule } from '../auth/auth.module';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    AuthModule,
+  ],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [
+    UsersService,
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+  ],
+})
+export class UsersModule {}
